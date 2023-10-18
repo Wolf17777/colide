576f49193e040905fe9ff99e2c35cf94d0caa630
Root <root@comonoid.com>
2023-10-18 17:03:30 +0200

    Changed misrepresentative class name.

--- apps.py
+++ apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 import os
 
-class DevEditorConfig(AppConfig):
+class ColideConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     # name should be the directory path where apps.py lies in relation to the django root folder with . as seperator
     name = os.path.abspath(__file__)[len(os.getcwd())+1:-len('apps.py')-1].replace('/','.')
